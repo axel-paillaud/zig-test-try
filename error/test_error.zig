beb4eeb74efeefe201b2ee9556365d841269bd15
Axel Paillaud <contact@axelweb.fr>
2025-02-13 19:43:45 +0100

    error, defer

---
+++ error/test_error.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+const FileOpenError = error{ AccessDenied, OutOfMemory, FileNotFound };
+
+const AllocationError = error{OutOfMemory};
+
+test "coerce error from a subset to a superset" {
+    const err: FileOpenError = AllocationError.OutOfMemory;
+    try expect(err == FileOpenError.OutOfMemory);
+}
