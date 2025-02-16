b8426a418a6a297d2f1f8347832996a24c3c34cf
Axel Paillaud <contact@axelweb.fr>
2025-02-16 17:26:22 +0100

    err inferred

--- error/test_error.zig
+++ error/test_error.zig
@@ -64,5 +64,7 @@ fn createFile() !void {
 test "inferred error test" {
     const x: error{AccessDenied}!void = createFile();
 
+    //Zig does not let us ignore error unions via _ = x;
+    //we must unwrap it with "try", "catch", or "if" by any means
     _ = x catch {};
 }
