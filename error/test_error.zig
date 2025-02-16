426c831795dccd9a2fc47f79e563841c464181d5
Axel Paillaud <contact@axelweb.fr>
2025-02-16 16:54:50 +0100

    error

--- error/test_error.zig
+++ error/test_error.zig
@@ -9,3 +9,10 @@ test "coerce error from a subset to a superset" {
     const err: FileOpenError = AllocationError.OutOfMemory;
     try expect(err == FileOpenError.OutOfMemory);
 }
+
+test "error union" {
+    const maybe_error: AllocationError!u16 = 10;
+    const no_error = maybe_error catch 0;
+
+    try expect(@TypeOf(no_error) == u16);
+}
